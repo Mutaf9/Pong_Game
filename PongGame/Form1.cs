3c966d69bc43f2a8cb9ecbe6233f0e883f3e78fd
Mutaf9 <safamutaf9@gamil.com>
2023-07-20 16:28:59 +0300

    unused comment line deleted

--- PongGame/Form1.cs
+++ PongGame/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
-// hersey tamam sadece skor hatasi duzeltilicek ve baslma ekrani eklenicek
+
 namespace PongGame
 {
     public partial class Form1 : Form
